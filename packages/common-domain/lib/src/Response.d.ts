94366514f705653454871e628bd4ca0c56fa63bd
susonthapa <33973551+susonthapa@users.noreply.github.com>
2022-05-07 06:18:39 +0545

    removed lib from tracking

--- packages/common-domain/lib/src/Response.d.ts
+++
@@ -1,7 +0,0 @@
-export declare type ApiResponse = {
-    code: number;
-    message: string;
-    data?: any;
-};
-export declare const success: ({ message, data }: Omit<Partial<ApiResponse>, 'code'>) => ApiResponse;
-export declare const failure: ({ message, code }: Partial<ApiResponse>) => ApiResponse;
